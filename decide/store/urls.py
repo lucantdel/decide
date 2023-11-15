d97a4a9091938fce27b4b5ed74afb149e5e928ff
Alvaro Sevilla Cabrera <80523208+Alvaroyiyo@users.noreply.github.com>
2023-11-15 18:18:49 +0100

    cambios en store
    Update decide/store/urls.py

--- decide/store/urls.py
+++ decide/store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.StoreView.as_view(), name='store'),
+    path("", views.StoreView.as_view(), name="store"), 
+    path("", views.StoreByPreferenceView.as_view(), name="storebypreference"),
 ]
