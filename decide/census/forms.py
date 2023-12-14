1d2facac07997a4280269125243b466eb6db3735
[[guillermo]] <[[ggrguilleggr@gmail.com]]>
2023-12-14 16:35:18 +0100

    fix: arreglo test

--- decide/census/forms.py
+++ decide/census/forms.py
@@ -6,13 +6,13 @@ class CreationCensusForm(forms.Form):
     voting_id = forms.IntegerField()
     voter_id = forms.IntegerField()
 
-    GENDER = [
+    GENDER_CHOICES = [
         ("MA", "Male"),
         ("FE", "Female"),
         ("NP", "No response")
     ]
     
-    CITY = [
+    CITY_CHOICES = [
         ("SE", "Sevilla"),
         ("MA", "Madrid"),
         ("BAR", "Barcelona"),
@@ -21,11 +21,10 @@ class CreationCensusForm(forms.Form):
     ]
 
     born_date = forms.DateField()
-    gender = forms.CharField(max_length=2, choices=GENDER, null=True)
-    city = forms.CharField(max_length=20, choices=CITY, null=True)
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, required=False)
+    city = forms.ChoiceField(choices=CITY_CHOICES, required=False)
 
     class Meta:
-
         model = Census 
         fields = (
             'voting_id', 
@@ -35,14 +34,13 @@ class CreationCensusForm(forms.Form):
             'city'
         )
 
-    def save (self, commit = True):
-
-        census = super(CreationCensusForm, self).save(commit = False)
-        census.voting_id= self.cleaned_data['voting_id']
-        census.voter_id= self.cleaned_data['voter_id']
-        census.born_date= self.cleaned_data['born_date']
-        census.gender= self.cleaned_data['gender']
-        census.city= self.cleaned_data['city']
+    def save(self, commit=True):
+        census = super(CreationCensusForm, self).save(commit=False)
+        census.voting_id = self.cleaned_data['voting_id']
+        census.voter_id = self.cleaned_data['voter_id']
+        census.born_date = self.cleaned_data['born_date']
+        census.gender = self.cleaned_data['gender']
+        census.city = self.cleaned_data['city']
 
         if commit:
             census.save()
