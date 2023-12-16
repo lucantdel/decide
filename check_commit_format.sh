74cb89e034103709bcee2b5858ecfffe1a4b5fc9
lucantdel <lucantdel@alum.us.es>
2023-12-16 17:48:23 +0100

    feat: mensaje error comprobacion mensajes

--- check_commit_format.sh
+++ check_commit_format.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Obtener los mensajes de commit desde el último push
-commit_messages=$(git log --format=%B -n 1 HEAD)
+# Obtener el mensaje de commit más reciente
+commit_message=$(git log --format=%B -n 1 HEAD)
 
 # Verificar si el mensaje de commit sigue el formato
-if [[ $commit_messages =~ ^(feat|fix|research|refactor|docs|test|conf): ]]; then
+if [[ $commit_message =~ ^(feat|fix|research|refactor|docs|test|conf): ]]; then
     echo "El formato de commit es correcto."
 else
-    echo "Error: Los mensajes de commit no cumplen con el formato esperado."
+    echo "Error: El mensaje de commit no cumple con el formato esperado."
+    echo "Mensaje de commit problemático: '$commit_message'"
     exit 1
 fi
